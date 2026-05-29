ca0db574011355f5d12ed78078850277846d5c94
SIMRAN SHARMA <lovelysharma2535@gmail.com>
2026-05-29 23:41:46 +0530

    hgj

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
 import { BrowserRouter } from 'react-router-dom'
 import React from 'react'
 import { Toaster } from 'react-hot-toast'
+import App from './App'
 
 createRoot(document.getElementById('root')).render(
  
     <BrowserRouter>
         <Toaster position="top-center" />
-
     <App />
     </BrowserRouter>
  
